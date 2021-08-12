58ef6b8d9e7d94ed35f1317099ae36c4b77e0ef9
sarahmatishoi <sarahmatishoi@gmail.com>
2021-08-12 06:44:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// const slideshow=document.querySelector(".slideshow")
+// setInterval(()=>{
+//   const firstitem=slideshow.firstElementChild
+//   firstitem.classList.add(faded-out)
+//   slideshow.removeChild(firstitem)
+//   slideshow.appendChild(firstitem)
