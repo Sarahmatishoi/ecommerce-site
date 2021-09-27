db5ffd87a307af1ebe6b8fb3e428e029b5778951
sarahmatishoi <sarahmatishoi@gmail.com>
2021-09-27 18:36:27 +0300

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-// const slideshow=document.querySelector(".slideshow")
-// setInterval(()=>{
-//   const firstitem=slideshow.firstElementChild
-//   firstitem.classList.add(faded-out)
-//   slideshow.removeChild(firstitem)
-//   slideshow.appendChild(firstitem)
+(firstitem)
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', loaded)
+} else {
+    loaded()
+}
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+function testJS()
+{
+
+var b = document.getElementById('card-text').value
+
+document.getElementById('cart-price').innerHTML = b;
+
+}
+function allItems(){
+    var x=document.getElementById('popupForm')
+    var b=[]
+    b.append(b)
+    document.getElementById('quantity').innerHTML=b.length()
+}
+function closePay() {
+    alert("Thank you for shopping with us")
+    return (true)
+}
+
+function topFunction(){
+    document.documentElement.scrollTop=0;
+}
+
+function openW(){
+    open("#c2",'billingform')
+}
